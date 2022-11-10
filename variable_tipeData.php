bc3e336d0afbb40afb809fe157af5678387c63ec
feriardianda <113500979+feriardianda@users.noreply.github.com>
2022-11-10 11:33:09 +0700

    Add files via upload

---
+++ variable_tipeData.php
@@ -0,0 +1,16 @@
+<?php
+
+$nrp = "173040888";
+$nama = "loren ipsum";
+$umur = 20;
+$nilai = 75,75;
+
+echo "nrp  : ". $nrp . "</br> ;
+echo "nama : $nama<?br>";
+print "umur : ".$umur;" print "</br>";
+// menampilkan bilangan real dengan format3 digit dibelakang koma
+print("Nilai : %.3f</br>, $nilai");
+
+
+
+?>
